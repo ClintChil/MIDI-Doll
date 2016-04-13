f2808b55d316e6dc768171eb728b0d0b81264770
Clint Chilcott <clintchil@gmail.com>
2016-04-13 15:11:36 -0700

    add AudioKit

---
+++ MIDI Doll/Pods/Target Support Files/Pods-MIDI Doll/Pods-MIDI Doll-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MIDI_Doll : NSObject
+@end
+@implementation PodsDummy_Pods_MIDI_Doll
+@end
---
+++ MIDI Doll/Pods/Target Support Files/Pods-MIDI Doll/Pods-MIDI Doll-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_MIDI_DollVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_MIDI_DollVersionString[];
+
